ebf9e85e8d6593e3673af278dd91fb91dc8d39a8
Lucas Sousa <lucasns241@gmail.com>
2023-06-05 23:00:10 -0400

    master

---
+++ FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Lucas \nI am 27 years old \nMy hometown is Boynton Beach, FL");
+    }
+}
